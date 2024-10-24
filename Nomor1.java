5ad2191055e923139a7b769ce16ba8f235760027
SabdaPutraAribawa <aribawap082@gmail.com>
2024-10-24 12:12:17 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,5 +1,30 @@
 public class Nomor1{
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
 
+        
+        System.out.print("Masukkan total jam lembur anda : ");
+        double totalJamLembur = scanner.nextDouble();
+
+        
+        double honor_lembur = hitungHonorLembur(totalJamLembur);
+
+       
+        System.out.println("Total jam lembur: " + totalJamLembur);
+        System.out.println("Honor lembur: " + honor_lembur);
+    }
+
+    public static double hitungHonorLembur (double totalJamLembur) {
+        double honor_lembur = 0;
+
+        if (totalJamLembur <= 16) {
+            honor_lembur = totalJamLembur * 10000;
+        } else {
+            honor_lembur = (16 * 10000) + (totalJamLembur - 16 * 15000);
+        }
+
+        return honor_lembur;
     }
 }
+
+    
